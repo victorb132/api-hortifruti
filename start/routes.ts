baa07148471f0d85de109d08cbd35dae669fed26
Victor Novais <victor96novais@gmail.com>
2023-11-07 17:56:34 -0300

    feat/migrations

--- start/routes.ts
+++ start/routes.ts
@@ -1,35 +1,7 @@
-import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Route from '@ioc:Adonis/Core/Route'
-import User from 'App/Models/User'
 
 Route.get('/', async () => {
-  User.create({
-    email: 'admin@hortifruti.com',
-    password: '123456',
-    type: 'admin'
-  });
-});
-
-Route.post('/gettoken', async ({ request, response, auth }: HttpContextContract) => {
-  const email = request.input('email');
-  const password = request.input('password');
-
-  const user = await User.findByOrFail('email', email);
-
-  if (user == null) {
-    return response.notFound('Usuário não encontrado');
-  }
-
-  const token = await auth.use('api').attempt(email, password);
-
-  return response.ok(token);
-});
-
-
-Route.get('/auth', async ({ auth, response }: HttpContextContract) => {
-  const userAuth = await auth.use('api').authenticate();
-
-  if (userAuth) {
-    return response.ok(userAuth);
+  return {
+    hortifruti: 'API Hortifruti',
   }
 });
