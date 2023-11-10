46ee2d33578032fa2b7e628ee9e1fda23a69541c
Victor Novais <victor96novais@gmail.com>
2023-11-10 12:13:50 -0300

    feat/seeders

--- database/seeders/03Company.ts
+++ database/seeders/03Company.ts
@@ -1,6 +1,7 @@
 import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
 import Company from 'App/Models/Company'
 import User from 'App/Models/User'
+import { faker } from '@faker-js/faker'
 
 export default class extends BaseSeeder {
   public async run() {
@@ -17,5 +18,23 @@ export default class extends BaseSeeder {
       blocked: false,
       userId: user.id
     })
+
+    for (let i = 2; i <= 20; i++) {
+      await User.create({
+        email: `company${i}@email.com`,
+        password: '12345678',
+        type: 'company',
+      })
+    }
+
+    for (let i = 2; i <= 20; i++) {
+      await Company.create({
+        name: `Company ${i}`,
+        logo: `https://picsum.photos/id/${i}/200/200`,
+        online: true,
+        blocked: false,
+        userId: i,
+      })
+    }
   }
 }
